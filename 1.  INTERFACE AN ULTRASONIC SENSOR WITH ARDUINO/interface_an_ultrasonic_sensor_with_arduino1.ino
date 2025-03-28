cb8f07a3af2bd6ae5f05fd92210a89d86d0d9de4
SRSohan021 <mdshahriarrahmansohan@gmail.com>
2025-03-29 01:08:19 +0600

    Add files via upload

---
+++ 1.  INTERFACE AN ULTRASONIC SENSOR WITH ARDUINO/interface_an_ultrasonic_sensor_with_arduino1.ino
@@ -0,0 +1,34 @@
+const int trigPin = 12;
+const int echoPin = 10;
+long duration;
+int distance;
+
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // Clear the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  
+  // Set the trigPin HIGH for 10 microseconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  // Read echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  
+  // Calculate the distance in cm
+  distance = duration * 0.034 / 2;
+  
+  // Print the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+  
+  delay(500); // Delay between measurements
+}
